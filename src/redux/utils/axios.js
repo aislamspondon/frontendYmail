d453c5b3c416cd04914497eadc6058a84a93ede5
farrukh-ahm <farrukhahm@gmail.com>
2023-02-27 09:27:34 +0600

    First

--- src/redux/utils/axios.js
+++ src/redux/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "http://3.115.193.230:8080/",
 });
 
 export default axiosInstance;
